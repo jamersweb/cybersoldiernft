d4bcda28f17ea07bda78052fcec5e497ea9573a9
Naveed Ullah <naveedullahhere@gmail.com>
2022-06-29 23:26:46 +0500

    Wowo

--- pages/components/SmoothScroll.js
+++ pages/components/SmoothScroll.js
@@ -1,19 +1,19 @@
-import Scrollbar from 'smooth-scrollbar';
-import { useEffect } from 'react';
+// import Scrollbar from 'smooth-scrollbar';
+// import { useEffect } from 'react';
 
 
 
-const options = {
-damping: 0.08,
-}
+// const options = {
+// damping: 0.08,
+// }
 
-const SmoothScroll = () => {
+// const SmoothScroll = () => {
 
-    Scrollbar.init(document.querySelector('#my_smooth_site'));
-    // useEffect(() => {
-    // }, [])
+//     Scrollbar.init(document.querySelector('#my_smooth_site'));
+//     // useEffect(() => {
+//     // }, [])
 
-    return null;
-}
+//     return null;
+// }
 
-export default SmoothScroll;
+// export default SmoothScroll;
--- pages/index.js
+++ pages/index.js
@@ -2,8 +2,8 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
-import { useLocomotiveScroll } from 'react-locomotive-scroll'
-import { LocomotiveScrollProvider } from 'react-locomotive-scroll'
+// import { useLocomotiveScroll } from 'react-locomotive-scroll'
+// import { LocomotiveScrollProvider } from 'react-locomotive-scroll'
 
 
 
