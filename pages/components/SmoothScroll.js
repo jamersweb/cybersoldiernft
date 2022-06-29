2fd90b0fc0f9bb1ff27cec3f9c1c354c36fd464c
Mannan <aptechlearners9@gmail.com>
2022-06-29 20:25:18 +0500

    UI Update (Abdul Mannan)

---
+++ pages/components/SmoothScroll.js
@@ -0,0 +1,19 @@
+import Scrollbar from 'smooth-scrollbar';
+import { useEffect } from 'react';
+
+
+
+const options = {
+damping: 0.08,
+}
+
+const SmoothScroll = () => {
+
+    Scrollbar.init(document.querySelector('#my_smooth_site'));
+    // useEffect(() => {
+    // }, [])
+
+    return null;
+}
+
+export default SmoothScroll;
