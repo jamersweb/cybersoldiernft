a960a5abb6e7cb83fe85e9ec0b7459f4bfbdb8bc
Mannan <aptechlearners9@gmail.com>
2022-06-30 17:33:00 +0500

    ...

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,6 @@
 import Head from 'next/head';
 import Script from 'next/script';
-import '../styles/globals.css'
-import { useEffect } from "react";
-// import AOS from "aos";
-// import "aos/dist/aos.css";
-// import "../scss/style.scss";
+import '../styles/globals.css' 
 
 function MyApp({ Component, pageProps }) {
 
@@ -18,8 +14,6 @@ function MyApp({ Component, pageProps }) {
 
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="stylesheet" href="https://unpkg.com/aos@next/dist/aos.css" />
-        {/* <script src="https://kit.fontawesome.com/20b4506959.js" crossorigin="anonymous"></script> */}
-
         <link
           href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet"
           integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1"
--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,9 @@
-import React, { useEffect } from "react";
-import Head from 'next/head'
+ import Head from 'next/head'
 import Image from 'next/image'
-import styles from '../styles/Home.module.css'
-// import AOS from "aos";
-// import "aos/dist/aos.css";
+import styles from '../styles/Home.module.css' 
 
 
 export default function Home() {
-
-  // useEffect(() => {
-  //   AOS.init();
-  // }, [])
-
 
   return (
     <>
